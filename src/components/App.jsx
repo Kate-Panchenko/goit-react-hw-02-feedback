abeb0f787b9bc94e0a8249de898cf319cb70bc95
Kate Panchenko <k.panchenko12345@gmail.com>
2023-02-28 21:46:46 +0200

    Step 4 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import { Component } from "react";
 import { Statistics } from "./Statistics/Statistics";
 import { FeedbackOptions } from "components/FeedbackOptions/FeedbackOptions";
 import { Section } from "./Section/Section";
+import { Notification } from "./Notification/Notification";
 
 export class App extends Component {
   state = {
@@ -40,20 +41,23 @@ export class App extends Component {
     return (
     <Layout>
         <GlobalStyle />
+
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={options}
             onLeaveFeedback={this.handleFeedback}
           />
         </Section>
+
         <Section title="Statistics">
-          <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
+          {this.countTotalFeedback() > 0 ?
+            (<Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
             positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
+          />) : (<Notification message="There is no feedback"/>)}
         </Section>
     </Layout>
   );
